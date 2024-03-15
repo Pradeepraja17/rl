8423eca7b63f94043e38ac10f0f5a2c4a8c1a07d
pradeep k <143427169+Pradeepraja17@users.noreply.github.com>
2024-03-15 10:51:42 +0530

    createframe.r

--- createframe.r
+++ createframe.r
@@ -7,14 +7,12 @@ employee_data <- data.frame(
   Mobile_Number = c("1234567890","1234567890", "9876543210", "4567890123", "7890123456", "2345678901")
 )
 
-# Step 2: Preprocess the dataset
-# In this example, we don't have any preprocessing steps since the dataset is clean.
+# Step 2: Preprocess the datase
 mean_sal<-mean(employee_data$Salary,na.rm = TRUE)
-
 employee_data$Salary[is.na(employee_data$Salary)]<-mean_sal
-
 # View the dataset
 print(employee_data)
+
 
 # Remove duplicate rows based on Employee_ID and Name columns
 employee_data_unique <- employee_data[!duplicated(employee_data[c("Employee_ID", "Name")]), ]
